03a26b938990ebf93cd0e3c9c531a05b286bf52f
Viktor <petrovskyvitya9@gmail.com>
2024-02-23 03:34:05 +0300

    Add class diagram

---
+++ messenger/bin/messenger.dart
@@ -0,0 +1,5 @@
+import 'package:messenger/messenger.dart' as messenger;
+
+void main(List<String> arguments) {
+  print('Hello world: ${messenger.calculate()}!');
+}
---
+++ messenger/test/messenger_test.dart
@@ -0,0 +1,8 @@
+import 'package:messenger/messenger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
